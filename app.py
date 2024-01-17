aff4fa1ab8217a9cb9f12338a0d1b63a52a66d10
boskin28 <150635516+boskin28@users.noreply.github.com>
2024-01-16 20:42:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ PINECONE_API_KEY = st.secrets['PINECONE_API_KEY']
 PINECONE_API_ENV = "us-east4-gcp"
 index_name = st.secrets['INDEX_NAME']
 
-pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+Pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 
 # Initilize OpenAI
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
