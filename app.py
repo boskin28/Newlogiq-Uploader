8a0b14f5915dc80f6d6c105a527321f4a5d50d07
boskin28 <150635516+boskin28@users.noreply.github.com>
2024-01-16 21:12:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pinecone import Pinecone
+import pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from PyPDF2 import PdfReader
@@ -57,7 +57,7 @@ PINECONE_API_KEY = st.secrets['PINECONE_API_KEY']
 PINECONE_API_ENV = "us-east4-gcp"
 index_name = st.secrets['INDEX_NAME']
 
-Pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
+pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
 
 # Initilize OpenAI
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
